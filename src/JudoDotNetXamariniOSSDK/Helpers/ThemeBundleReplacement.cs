38cc9ee38758bfbead163ada74c44de1abc39821
Igor Candido <igor.candido@dummy.macs.judo>
2015-03-26 15:37:31 +0000

    Added start of ThemeBundleReplacement

--- src/JudoDotNetXamariniOSSDK/Helpers/ThemeBundleReplacement.cs
+++ src/JudoDotNetXamariniOSSDK/Helpers/ThemeBundleReplacement.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using UIKit;
 
 namespace JudoDotNetXamariniOSSDK
 {
-	public class ThemeBundleReplacement
+	public enum BundledOrReplacement { Bundled, Replacement };
+
+	public static class ThemeBundleReplacement
 	{
-		public ThemeBundleReplacement ()
+
+		public static UIImage BundledOrReplacement(string imageName, BundledOrReplacement bundledOrReplacement)
 		{
+			
 		}
+
+		public static string BundledOrReplacement(string stringName, BundledOrReplacement bundledOrReplacement)
+		{
+
+		}
+
+		public static UIColor BundledOrReplacement(string colourName, BundledOrReplacement bundledOrReplacement)
+		{
+
+		}
+
 	}
 }
 
