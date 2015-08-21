d43a65d5a7d8e52f2f42758059bcd59d76f3926b
Luke Fieldsend <luke.fieldsend@judopayments.com>
2015-08-21 14:58:48 +0100

    missed some files

--- src/JudoDotNetXamariniOSSDK/Views/TokenPaymentView.cs
+++ src/JudoDotNetXamariniOSSDK/Views/TokenPaymentView.cs
@@ -101,7 +101,10 @@ namespace JudoDotNetXamariniOSSDK
 		private void UpdateUI ()
 		{	
 			PaymentButton.Enabled = tokenCell.Complete;
-			PaymentButton.Hidden = !tokenCell.Complete;
+			PaymentButton.Hidden = !thel;lo world
+			io;'jsakflsdajfie jfjsadf 
+			\
+			dsafkje984389ljfaksfjsdf kdsjrew90klsdklg  dksfl;okenCell.Complete;
 			if (tokenCell.Complete) {
 				DismissKeyboardAction ();
 			}
