86b33e5bb9117bc8d09df1b1cc88997f84e43175
Luke Fieldsend <luke.fieldsend@judopayments.com>
2015-09-07 16:52:57 +0100

    moved CreditCardViewOver to the new generic cell system

--- src/JudoDotNetXamariniOSSDK/Views/CreditCardView.designer.cs
+++ src/JudoDotNetXamariniOSSDK/Views/CreditCardView.designer.cs
@@ -13,129 +13,19 @@ namespace JudoDotNetXamariniOSSDK
 	partial class CreditCardView
 	{
 		[Outlet]
-		UIKit.UITableViewCell AVSCell { get; set; }
-
-		[Outlet]
-		UIKit.UIButton CancelButton { get; set; }
-
-		[Outlet]
-		UIKit.UITableViewCell CardDetailCell { get; set; }
-
-		[Outlet]
-		UIKit.UITextView ccText { get; set; }
-
-		[Outlet]
-		UIKit.UIView containerView { get; set; }
-
-		[Outlet]
-		UIKit.UIButton CountryButton { get; set; }
-
-		[Outlet]
-		UIKit.UILabel CountryLabel { get; set; }
-
-		[Outlet]
-		UIKit.UIImageView creditCardImage { get; set; }
-
-		[Outlet]
 		UIKit.UIView EncapsulatingView { get; set; }
-
-		[Outlet]
-		UIKit.UIButton ExpiryInfoButton { get; set; }
-
-		[Outlet]
-		UIKit.UIButton HomeButton { get; set; }
-
-		[Outlet]
-		UIKit.UITextField IssueNumberTextField { get; set; }
-
-		[Outlet]
-		UIKit.UITableViewCell MaestroCell { get; set; }
-
-		[Outlet]
-		UIKit.UITableViewCell PayCell { get; set; }
-
-		[Outlet]
-		UIKit.UILabel PaymentErrorLabel { get; set; }
-
-		[Outlet]
-		JudoDotNetXamariniOSSDK.PlaceHolderTextView placeView { get; set; }
-
-		[Outlet]
-		UIKit.UIView PostCodeContainerView { get; set; }
-
-		[Outlet]
-		UIKit.UITextField PostcodeTextField { get; set; }
-
-		[Outlet]
-		UIKit.UITableViewCell ReassuringTextCell { get; set; }
-
-		[Outlet]
-		UIKit.UITableViewCell SpacerCell { get; set; }
-
-		[Outlet]
-		UIKit.UITextField StartDateTextField { get; set; }
-
-		[Outlet]
-		UIKit.UILabel StartDateWarningLabel { get; set; }
-
-		[Outlet]
-		UIKit.UILabel StateDatePlaceholder { get; set; }
-
-		[Outlet]
-		UIKit.UILabel StatusHelpLabel { get; set; }
 
 		[Outlet]
 		UIKit.UIButton SubmitButton { get; set; }
 
 		[Outlet]
-		UIKit.NSLayoutConstraint TableHeight { get; set; }
-
-		[Outlet]
 		UIKit.UITableView TableView { get; set; }
 
-		[Outlet]
-		UIKit.UIScrollView textScroller { get; set; }
-		
 		void ReleaseDesignerOutlets ()
 		{
-			if (AVSCell != null) {
-				AVSCell.Dispose ();
-				AVSCell = null;
-			}
-
-			if (CancelButton != null) {
-				CancelButton.Dispose ();
-				CancelButton = null;
-			}
-
-			if (CardDetailCell != null) {
-				CardDetailCell.Dispose ();
-				CardDetailCell = null;
-			}
-
-			if (ccText != null) {
-				ccText.Dispose ();
-				ccText = null;
-			}
-
-			if (containerView != null) {
-				containerView.Dispose ();
-				containerView = null;
-			}
-
-			if (CountryButton != null) {
-				CountryButton.Dispose ();
-				CountryButton = null;
-			}
-
-			if (CountryLabel != null) {
-				CountryLabel.Dispose ();
-				CountryLabel = null;
-			}
-
-			if (creditCardImage != null) {
-				creditCardImage.Dispose ();
-				creditCardImage = null;
+			if (SubmitButton != null) {
+				SubmitButton.Dispose ();
+				SubmitButton = null;
 			}
 
 			if (EncapsulatingView != null) {
@@ -143,99 +33,9 @@ namespace JudoDotNetXamariniOSSDK
 				EncapsulatingView = null;
 			}
 
-			if (ExpiryInfoButton != null) {
-				ExpiryInfoButton.Dispose ();
-				ExpiryInfoButton = null;
-			}
-
-			if (HomeButton != null) {
-				HomeButton.Dispose ();
-				HomeButton = null;
-			}
-
-			if (IssueNumberTextField != null) {
-				IssueNumberTextField.Dispose ();
-				IssueNumberTextField = null;
-			}
-
-			if (MaestroCell != null) {
-				MaestroCell.Dispose ();
-				MaestroCell = null;
-			}
-
-			if (PayCell != null) {
-				PayCell.Dispose ();
-				PayCell = null;
-			}
-
-			if (PaymentErrorLabel != null) {
-				PaymentErrorLabel.Dispose ();
-				PaymentErrorLabel = null;
-			}
-
-			if (placeView != null) {
-				placeView.Dispose ();
-				placeView = null;
-			}
-
-			if (PostCodeContainerView != null) {
-				PostCodeContainerView.Dispose ();
-				PostCodeContainerView = null;
-			}
-
-			if (PostcodeTextField != null) {
-				PostcodeTextField.Dispose ();
-				PostcodeTextField = null;
-			}
-
-			if (ReassuringTextCell != null) {
-				ReassuringTextCell.Dispose ();
-				ReassuringTextCell = null;
-			}
-
-			if (SpacerCell != null) {
-				SpacerCell.Dispose ();
-				SpacerCell = null;
-			}
-
-			if (StartDateTextField != null) {
-				StartDateTextField.Dispose ();
-				StartDateTextField = null;
-			}
-
-			if (StartDateWarningLabel != null) {
-				StartDateWarningLabel.Dispose ();
-				StartDateWarningLabel = null;
-			}
-
-			if (StateDatePlaceholder != null) {
-				StateDatePlaceholder.Dispose ();
-				StateDatePlaceholder = null;
-			}
-
-			if (StatusHelpLabel != null) {
-				StatusHelpLabel.Dispose ();
-				StatusHelpLabel = null;
-			}
-
-			if (SubmitButton != null) {
-				SubmitButton.Dispose ();
-				SubmitButton = null;
-			}
-
-			if (TableHeight != null) {
-				TableHeight.Dispose ();
-				TableHeight = null;
-			}
-
 			if (TableView != null) {
 				TableView.Dispose ();
 				TableView = null;
-			}
-
-			if (textScroller != null) {
-				textScroller.Dispose ();
-				textScroller = null;
 			}
 		}
 	}
