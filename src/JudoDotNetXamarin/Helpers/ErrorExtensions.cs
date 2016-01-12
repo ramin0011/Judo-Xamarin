01950ce40e05c5d3e893c1f386cc96e979b8b9d5
Luke Fieldsend <luke.fieldsend@judopayments.com>
2016-01-12 11:31:30 +0000

    removed some commented out code

--- src/JudoDotNetXamarin/Helpers/ErrorExtensions.cs
+++ src/JudoDotNetXamarin/Helpers/ErrorExtensions.cs
@@ -6,24 +6,6 @@ namespace JudoDotNetXamarin
 {
     public static class ErrorExtensions
     {
-        public static void FlattenToJudoFailure (this ModelError error, JudoFailureCallback failureCallback)
-        {
-            if (failureCallback != null) {
-
-////
-////                List<FieldError> models = new List<FieldError> (); 
-////                foreach (FieldError em in error.ModelErrors) {
-////                    models.Add (new FieldError (){ Message = em.Message,FieldName = em.FieldName, });
-////                }
-//                var judoError = new JudoError () {
-//                    Exception = error.Message,
-//                    ApiError = error
-//                };
-//
-//                failureCallback (judoError);
-
-            }
-        }
 
         public static JudoError FlattenToJudoError (this AggregateException errors)
         {
